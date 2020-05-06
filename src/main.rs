581fb39394c398357c31359feba6e0a62bfca4f3
t-yng <t.yng.greenmonkey@gmail.com>
2020-05-06 16:31:14 +0900

    dotenvは不要になったので削除

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 extern crate bb;
-extern crate dotenv;
 
 use std::env;
 use std::process;
-use dotenv::dotenv;
 use bb::cmd::run;
 use bb::config::Config;
 use bb::args::Args;
 
 fn main() {
-    dotenv().ok();
-
     let args: Vec<String> = env::args().collect();
     let args = Args::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments {}", err);
